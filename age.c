5e86b57cc123a0c029c653467a87be7beaa32561
SerenityMonk <srzoom98@gmail.com>
2026-01-22 10:43:37 +0530

    Create age.c

---
+++ age.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Enter your age:");
+    scanf("%d", &age);
+    printf("Your age is: %d", age);
+    return 0;
+}
+
+OUTPUT:
+Enter your age:20
+Your age is: 20
