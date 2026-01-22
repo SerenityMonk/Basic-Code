779580032ce7ef0332a87ec0b5d01d5aa308b3b0
SerenityMonk <srzoom98@gmail.com>
2026-01-22 10:49:13 +0530

    Create datatypes.c

---
+++ datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    const int PI = 3;
+    printf("My age is %d my mark is %.2f I had %c dog %d years ago", a, b, c, PI);
+    return 0;
+}
+
+OUTPUT:
+My age is 10 my mark is 5.50 I had A dog 3 years ago
